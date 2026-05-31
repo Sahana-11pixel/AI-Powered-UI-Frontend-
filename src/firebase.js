fb0fbefcb1fb1743b233d70c13bfd3bdfe9a41ce
Sahana <sahanaselvasekar11@gmail.com>
2026-05-31 16:49:29 +0530

    Update in Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
-// Firebase SDK initialization
-// Docs: https://firebase.google.com/docs/web/setup
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBD64ElFqIIh-Cbz2j8ccKXj5R4dOA_egs",
-    authDomain: "ui-code-gen.firebaseapp.com",
-    projectId: "ui-code-gen",
-    storageBucket: "ui-code-gen.firebasestorage.app",
-    messagingSenderId: "508579672764",
-    appId: "1:508579672764:web:f4eaf928bb647e233c96b9",
-    measurementId: "G-DCRVWN36Q6"
+  apiKey: "AIzaSyDJGfGtD4gp0CcjPSYOUTbrUSKddhEY9Ws",
+  authDomain: "ai-powered-uicodegen.firebaseapp.com",
+  projectId: "ai-powered-uicodegen",
+  storageBucket: "ai-powered-uicodegen.firebasestorage.app",
+  messagingSenderId: "452849444466",
+  appId: "1:452849444466:web:759a4480bb4220384dc4eb",
+  measurementId: "G-4V81WN9W3J"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Export auth instance for use in signup/login pages
-export const auth = getAuth(app);
-export default app;
+
