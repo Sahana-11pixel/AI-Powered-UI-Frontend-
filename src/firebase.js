1642e011b3e9160f276a275c3be29c7cc342a5ac
Sahana <sahanaselvasekar11@gmail.com>
2026-05-31 17:08:38 +0530

    Remove:Ganalytics firebase

--- src/firebase.js
+++ src/firebase.js
@@ -8,19 +8,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJGfGtD4gp0CcjPSYOUTbrUSKddhEY9Ws",
-  authDomain: "ai-powered-uicodegen.firebaseapp.com",
-  projectId: "ai-powered-uicodegen",
-  storageBucket: "ai-powered-uicodegen.firebasestorage.app",
-  messagingSenderId: "452849444466",
-  appId: "1:452849444466:web:759a4480bb4220384dc4eb",
-  measurementId: "G-4V81WN9W3J"
+    apiKey: "AIzaSyDJGfGtD4gp0CcjPSYOUTbrUSKddhEY9Ws",
+    authDomain: "ai-powered-uicodegen.firebaseapp.com",
+    projectId: "ai-powered-uicodegen",
+    storageBucket: "ai-powered-uicodegen.firebasestorage.app",
+    messagingSenderId: "452849444466",
+    appId: "1:452849444466:web:759a4480bb4220384dc4eb",
+    measurementId: "G-4V81WN9W3J"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 // Export auth instance for use in signup/login pages
 export const auth = getAuth(app);
 export default app;
